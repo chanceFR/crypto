4d3f2bf0f969beaac704971497a86868d276cf25
romeo <lariviereromeo@gmail.com>
2022-09-27 13:41:25 +0200

    je suis la

--- src/main/java/DES.java
+++ src/main/java/DES.java
@@ -4,6 +4,13 @@ public class DES {
 
     }
     public int[] stringToBits(String s){
-        
+
+    }
+
+    public String bitsToString(int[] s){
+    }
+
+    public static void main(String[] args){
+        System.out.println("je suis la");
     }
 }
